87ceeaa0f70dcdf0ee72aea289b7a3af471044be
Yow-Bang (Darren) Wang <ybdarrenwang@gmail.com>
2016-07-20 21:47:33 -0700

    finally fixed scrollbar and size issue

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import sys, Tkinter
 sys.path.insert(0, "./gui/")
 from button import OpenFolderButton, NextBatchButton
 
-#Need to set a max size for frameTwo. Otherwise, it will grow as needed, and scrollbar do not act
+# Note: need to set size for bg_canvas here; otherwise it will grow disregard the size set while created!
 def AuxscrollFunction(event):
-    bg_canvas.configure(scrollregion=bg_canvas.bbox("all"))#,width=1000,height=250)
+    bg_canvas.configure(scrollregion=bg_canvas.bbox("all"), height=100)
 
 # create root
 root = Tkinter.Tk()
@@ -18,7 +18,7 @@ selected_photo_frame.pack(fill=Tkinter.BOTH, expand=True)
 
 # create background for scroll bar
 bg_frame=Tkinter.Frame(root, height=100)
-bg_frame.pack(fill="x", expand=True)
+bg_frame.pack(fill=Tkinter.BOTH, expand=True)
 bg_canvas = Tkinter.Canvas(bg_frame)
 xscrollbar = Tkinter.Scrollbar(bg_frame, orient="horizontal", command=bg_canvas.xview)
 xscrollbar.pack(side="bottom", fill="x")
@@ -27,10 +27,10 @@ bg_canvas.configure(xscrollcommand=xscrollbar.set)
 bg_canvas.pack(fill=Tkinter.BOTH, expand=True)
 
 # create frame for duplicated photo batch display
-batch_photo_frame = Tkinter.Frame(bg_canvas)
+batch_photo_frame = Tkinter.Frame(bg_canvas, height=100)
 bg_canvas.create_window((0,0),window=batch_photo_frame,anchor='nw')
 batch_photo_frame.bind("<Configure>", AuxscrollFunction)
-batch_photo_frame.pack()
+# Note: don't pack batch_photo_frame here, otherwise scroll bar won't show!!!
 
 # create buttons
 button1 = OpenFolderButton(root, batch_photo_frame)
