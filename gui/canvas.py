7722fac0f89ae7e761d23cbe54b7ec9ee6203730
Yow-Bang (Darren) Wang <ybdarrenwang@gmail.com>
2016-07-20 20:57:37 -0700

    Completed display big image functionality

--- gui/canvas.py
+++ gui/canvas.py
@@ -1,18 +1,21 @@
-import Tkinter, Tkconstants
+import Tkinter, Tkconstants, tkMessageBox
 from PIL import Image, ImageTk
 
 class Canvas(object):
-    def __init__(self, parent, w, h, isCentered=False):
+    def __init__(self, parent, w, h, canvas4display=None, img_path=None):
         self.width = w
         self.height = h
         self.canvas = Tkinter.Canvas(parent, width=w, height=h)
-        if isCentered:
+        if canvas4display==None:
             self.canvas.pack()
         else:
-            #self.canvas.bind("<Button-1>", callback)
+            self.canvas.bind("<Button-1>", lambda event, cv=canvas4display, img=img_path: self.showcaseImage(cv, img))
             self.canvas.pack(side=Tkinter.LEFT)
         self.img = [None]
         self.thumb = [None]
+
+    def showcaseImage(self, cv4display, img_path):
+        cv4display.loadImages(img_path)
 
     def loadImages(self, image):
         tmp = Image.open(image).resize((self.width, self.height), Image.ANTIALIAS)
--- gui/gui_methods.py
+++ gui/gui_methods.py
@@ -19,17 +19,17 @@ def getNextDuplicatedBatch(root, batch_photo_frame, selected_photo_frame, bt, ca
         return
     try:
         copies = bt.crawler[-1].next()
+        # show the first photo in selected_photo_frame
+        height, width, channel = copies[0]["shape"]
+        ratio = height/450.0
+        cache.append(Canvas(selected_photo_frame, int(width/ratio), int(height/ratio)))
+        cache[0].loadImages(copies[0]["path"])
         # show duplicated photos in batch_photo_frame
         for idx,cp in enumerate(copies):
             height, width, channel = cp["shape"]
             ratio = height/100.0
-            cache.append(Canvas(batch_photo_frame, int(width/ratio), int(height/ratio)))
+            cache.append(Canvas(batch_photo_frame, int(width/ratio), int(height/ratio), cache[0], cp["path"]))
             cache[-1].loadImages(cp["path"])
-        # show the first photo in selected_photo_frame
-        height, width, channel = copies[0]["shape"]
-        ratio = height/450.0
-        cache.append(Canvas(selected_photo_frame, int(width/ratio), int(height/ratio), True))
-        cache[-1].loadImages(copies[0]["path"])
         root.update_idletasks()
     except StopIteration:
         tkMessageBox.showinfo("Warning", "No more duplicated photos found.")
